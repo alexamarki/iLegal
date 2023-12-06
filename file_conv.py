27a7cc758b69b6bb4b76f8315f752a3bc45a8b0d
alexamarki <amarkin2006@gmail.com>
2023-12-06 10:45:30 +0300

    fjf

--- file_conv.py
+++ file_conv.py
@@ -2,6 +2,8 @@ from pdf2docx import Converter
 import docx
 import pymorphy3
 from docx.shared import RGBColor
+from docx2pdf import convert
+
 def change_text_color(doc_file, word, color):
     doc = docx.Document(doc_file)
     for paragraph in doc.paragraphs:
@@ -11,35 +13,51 @@ def change_text_color(doc_file, word, color):
                 run = paragraph.runs[i]
                 font = run.font
                 font.color.rgb = RGBColor(*color)
-morph = pymorphy3.MorphAnalyzer()
-cv = Converter("pdf1.pdf")
-cv.convert("Doc1.docx", start=0, end=None)
-cv.close()
-doc = docx.Document('Doc1.docx')
-key_words = ['обязан', 'штраф','расторгнут','одностороннем','передаваться']
-text = []
-att = []
-par = []
-key = []
-for paragraph in doc.paragraphs:
-    text.append(paragraph.text)
-    words = []
-    for i in paragraph.text.split():
-        words.append(morph.parse(i)[0][2].lower())
-    par.append(words)
-doc = docx.Document()
-for j in range(len(text)):
-    for i in key_words:
-        if morph.parse(i.lower())[0][2] in par[j] and text[j] not in att:
-            att.append(text[j])
-            paragraph = doc.add_paragraph()
-            sentence_split = text[j].split()
-            for x in sentence_split:
-                if x == text[j].split()[par[j].index(morph.parse(i.lower())[0][2])]:
-                    run2 = paragraph.add_run(x)
-                    red = RGBColor(255, 0, 0)
-                    run2.font.color.rgb = red
-                else:
-                    paragraph.add_run(x)
-                paragraph.add_run(" ")
-doc.save('Doc1.docx')
+
+def important_changer(filename, param):
+    morph = pymorphy3.MorphAnalyzer()
+    cv = Converter(filename)
+    cv.convert("Doc1.docx", start=0, end=None)
+    cv.close()
+    doc = docx.Document('Doc1.docx')
+    key_words = ['обязан', 'штраф','расторгнут','одностороннем','передаваться']
+    text = []
+    att = []
+    par = []
+    key = []
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+        words = []
+        for i in paragraph.text.split():
+            words.append(morph.parse(i)[0][2].lower())
+        par.append(words)
+    doc = docx.Document()
+    doc1 = docx.Document()
+    for j in range(len(text)):
+        for i in key_words:
+            if morph.parse(i.lower())[0][2] in par[j] and text[j] not in att:
+                att.append(text[j])
+                paragraph = doc.add_paragraph()
+                paragraph1 = doc1.add_paragraph()
+                sentence_split = text[j].split()
+                for x in sentence_split:
+                    if x == text[j].split()[par[j].index(morph.parse(i.lower())[0][2])]:
+                        run2 = paragraph.add_run(x)
+                        run3 = paragraph1.add_run(x)
+                        red = RGBColor(255, 0, 0)
+                        run2.font.color.rgb = red
+                        run3.font.color.rgb = red
+                    else:
+                        paragraph.add_run(x)
+                        paragraph1.add_run(x)
+                    paragraph.add_run(" ")
+                    paragraph1.add_run(" ")
+            else:
+                paragraph1 = doc1.add_paragraph()
+                paragraph1.add_run(text[j])
+                paragraph1.add_run(" ")
+
+    if param != 'highlight':
+        doc.save(f'{filename}.docx')
+    else:
+        doc1.save(f'{filename}.docx')
--- main.py
+++ main.py
@@ -1,54 +1,16 @@
 import os
-from flask import Flask, render_template, redirect, send_file
-from flask import request
+from flask import Flask, render_template, redirect, send_from_directory
+from flask import request, abort
 from werkzeug import exceptions
 from forms.search_forms import FileForm
 import random
-
-# ! ! ! ! ! ! ! ! ! ! ! ! ! Comment clarification ! ! ! ! ! ! ! ! ! ! ! ! ! #
-# This is ArtHeritage's main file, which includes all routes and the         #
-#   majority of the logic required for our Flask web app.                    #
-# This file in particular is commented in the following manner:              #
-# --Use case 1--                                                             #
-# | [code] # comment - a comment regarding a particular portion of the code, #
-#                          when it's written on the same line as the code.   #
-# | # > comment - the aforementioned use case, when written on a blank line. #
-# --Use case 2--                                                             #
-# | # '--' * n <tag> comment                                                 #
-# In use case #2, <tag> is either an opening or a closing tag.               #
-#   These tags are always used in pairs (<tag>code</tag>).                   #
-# * Tags in our comments surround code related to a particular portion of    #
-#       the app, and are supposed to improve code readability, as well as    #
-#       ease troubleshooting.                                                #
-# * Next to the opening tag is a comment, explaining the purpose of the code #
-#       it envelops.                                                         #
-# * '--' may also be used n times (n >= 0) in the beginning of a comment to  #
-#       clarify the level of indentation.                                    #
-# As such, the following pseudocode...                                       #
-# 01| # <tag1> Tag 1 explanation                                             #
-# 02| # -- <tag1-1> Tag 1.1 explanation                                      #
-# 03| def foo():                                                             #
-# 04|   pass                                                                 #
-# 05| # -- </tag1-1>                                                         #
-# 06| # -- <tag1-2> Tag 1.2 explanation                                      #
-# 07| def bar():                                                             #
-# 08|   pass                                                                 #
-# 09| # -- </tag1-2>                                                         #
-# 10| # </tag1>                                                              #
-# 11| # <tag2> Tag 2 explanation                                             #
-# 12| def baz():                                                             #
-# 13|   pass                                                                 #
-# 14| # </tag2>                                                              #
-# ...would tell us that foo() holds the properties of tags 1 and 1.1,        #
-#   bar() holds the properties of tags 1 and 1.2,                            #
-#   and baz() holds the property of tag 2                                    #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from file_conv import important_changer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'KJKjxkwh7w6%575&jBHJI(987'
-app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
-app.config['UPLOAD_FOLDER'] = '/static/img'
-
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024 * 1024
+UPLOADED_MANUALS_ALLOW = ('pdf',)
+app.config['UPLOAD_FOLDER'] = 'static/img/'
 
 
 # <handler> Login, error handling via Flask
@@ -83,11 +45,14 @@ app.register_error_handler(500, handle_500)
 def home():
     fa_form = FileForm()
     if fa_form.filedata.data:
-        file = fa_form.filedata.data
-        num = random.randint(10000000000, 99999999999)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'] + "\pdf\\", f'f{num}'))
-        # call file creator
-        return render_template('main.html', form=fa_form, filename=f'f{num}_edit', webview_title='iLegal main')
+        num = str(random.randint(10000000000, 99999999999))
+        manual_path = ''.join([
+            app.config['UPLOAD_FOLDER'],
+            num, '.pdf'
+        ])
+        fa_form.filedata.data.save(manual_path)
+        important_changer(manual_path, param=fa_form.on_display_at.data)
+        return render_template('main.html', form=fa_form, filename=f'{num}.pdf.docx', webview_title='iLegal main')
     return render_template('main.html', form=fa_form, webview_title='iLegal main')
 
 
@@ -96,9 +61,16 @@ def info():
     return render_template('info.html', webview_title='About iLegal')
 
 
-@app.route('/download/<string:file>')
-def download(file):
-    return send_file(file, as_attachment=True)
+@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    print(filename)
+    try:
+        print(app.config['UPLOAD_FOLDER'])
+        return send_from_directory(app.config['UPLOAD_FOLDER'], 'missing.png', as_attachment=True)
+    except Exception as e:
+        print('huh')
+        print(e)
+        abort(404)
 
 
 if __name__ == '__main__':
